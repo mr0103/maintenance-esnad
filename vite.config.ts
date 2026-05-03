376891fc2ecad1fa9de1a0239f4d5ddfe7573def
MR0103 <fhjuj02@gmail.com>
2026-05-03 11:04:58 +0300

    Set base path for GitHub deployment
    Add base path for GitHub deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    // هذا السطر أضفناه لكي يعمل الرابط على GitHub بنجاح
+    base: '/Maintenance-esnad/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
